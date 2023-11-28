b3bf67ff5fb001fb69dc90c78e6ca904bdf13b95
Churlygp <74980673+CARLYGP@users.noreply.github.com>
2023-11-28 01:29:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,106 +1,241 @@
 #include<iostream>
 #include<string>
 #include <cstring>//pata strslen
-#include<vector>
 #include<map>
+#include<fstream> // para hacer copia de seguridad en archivo
 #include"Hash_table.hpp"
 #include"Arbol_binario.hpp"
-#include<unordered_map>
+#include<vector>
+
+// el arbol sirve como historial de nombres
 
 using namespace std;
 
-
-int asignarValorLexicografico(const std::string& palabra) {
-    std::unordered_map<char, int> valorLetra;
-
-    // valores únicos a cada letra basado en su posición lexicográfica
-    for (char letra = 'a'; letra <= 'z'; ++letra) {
-        valorLetra[letra] = letra - 'a' + 1;
-    }
-
-    int valorTotal = 0;
-
-    // Calcula el valor total sumando los valores de cada letra en la palabra
-    for (char letra : palabra) {
-        letra = std::tolower(letra);
-        valorTotal += valorLetra[letra];
-    }
-
-    return valorTotal;
-}
-
-
 struct contacto{
-    int suma;
-    char nombre[100]; 
-    double telefono;
-    vector<string>redes_sociales;
-    // como hago el icono
+  
+    string nombre;
+    string telefono;
+    string redes_sociales;
+    string Instagram;
+    string Github; 
     string direccion;
+    string calle;
+    string numero;
+    string indicativo; //(-)
+    int veces_repetido;
+    int veces_visitado; //# de veces que se ha visitado en cada ejecución un perfil
 };
+
+
+
 // hacer que el valor que tenga cada nombre sea su indice en el vector ordenado
 int main(){
-    
-    Btree<int> mi_arbol;
-    HashMap<int> hash;
-
    
-    map<int,int>map1;  // que con la clave del vector ordenado buscar string asociado
-    map<int,contacto>map2;
+    map<string,contacto>datos_no_rep; //si solo se necesita el nombre para acceder al contacto
+    map<string,contacto>datos_rep2;  // se necesita el numero porque el nombre esta repetido
+    map<string,int>datos_rep; // mirar cuantas veces esta repetido el nombre
+    Btree<string>arbol_str;
+
+    arbol_str.display();
+    Btree<string> mi_arbol;
+    HashMap<string> hash; // string es el valor de la llave y int el valor 
+    
+   
+
     int exit=1;
-while(exit!=0){
+
+int opciones;
+int salir=1;
+
+while(salir==1){
+    cout<<"--------------------------Libreta de contactos---------------------------"<<endl;
+    cout<<"1:Agregar contacto"<<endl;
+     cout<<"2:Buscar contacto"<<endl;
+     cout<<"3:Eliminar contacto"<<endl;
+     cout<<"4:Mostrar lista completa de contactos"<<endl;
+     cout<<"5:Mostrar contactos ordenados por letra"<<endl;
+     cout<<"6:Mostrar cantidad de contactos en la lista"<<endl;
+     cout<<"7:Mostar lista de contactos con su frecuencia de busqueda"<<endl;
+     cout<< "8:Realizar copia de seguridad en archivo de texto"<<endl;
+     cout<<"9:Salir"<<endl; //opciones    
+    cin>>opciones;
+   // si oprime buscar contacto, mirar si quiere buscar por letra
+    if (opciones==1){
+        
     contacto c;
-    string n="";
+        
+    string nombre="";
     int count=0;
     cout<<"Ingrese su nombre"<<endl;
    cin>>c.nombre;
-
-    count=asignarValorLexicografico(c.nombre);
-
-    cout<<count;
-    c.suma=count;
+   cout<<"Ingrese su telefono";
+   cin>>c.telefono;
+   cout<<"Ingrese su direccion cada dato de su direccion: "<<endl;// hacer una estructura que guarde calle, #, - 
+   cout<<"Ingrese calle"<<endl;
+   cin>>c.calle;
+   cout<<"Ingrese numero(#)"<<endl;
+   cin>>c.numero;
+   cout<<"Ingrese (-)"<<endl;
+   cin>>c.indicativo;
+   c.direccion="calle" + c.calle + "#"+ c.numero+"-"+ c.indicativo; // concatenacion de direccion
+   cout<<"Usuario de Instagram"<<endl;
+   cin>> c.Instagram;
+   cout<<"Usuario de Github"<<endl;
+   cin>>c.Github;
+   
+   c.veces_repetido=0;
+    if (mi_arbol.find(c.nombre)==1){
+        c.veces_repetido+=1;
+    }
+    c.veces_visitado=0;
+   mi_arbol.insert(c.nombre);
+    hash.insert(c.nombre,c.telefono); 
+    // asignacion de nombre con contacto para que pueda ser variable el valor
     
-    map2[count]=c;
+    datos_no_rep[c.nombre]=c;
+    datos_rep2[c.telefono]=c; 
+    datos_rep[c.nombre]=c.veces_repetido;
     
-    mi_arbol.insert(count);
+    } if (opciones ==2){
+        string num;
+        string n;
+        cout<<"Ingrese nombre que quiere buscar"<<endl;
+        cin>>n;
+        
+        auto item = datos_rep.find(n);
+        if (item != datos_rep.end()) {
 
-cout<<"oprima 0 si quiere salir"<<endl;
-    cin>>exit;
-}
+        if( item->second>=1){
+            auto item = datos_rep2.find(num);
+                 if (item != datos_rep2.end()) {
+                     item->second.veces_visitado+=1; // si se ingresa un nombre repetido se aumenta el atributo veces_visitado
+                    
+                    }
+                    
+              cout<<"Se necesita informacion adicional, ingrese el numero del contacto"<<endl; // cuando el nombre es repetido
+              cin>>num;
+              int opc1;
+              cout<<"Perfecto, quiere saber info de " << n <<" sobre: (1:direccion)(2:redes sociales)(3:salir)"<<endl;
+              cin>>opc1;
+              // se va a mostrar la informacion de los contactos repetidos
+              if(opc1==1){
+                 auto item = datos_rep2.find(num);
+                 if (item != datos_rep2.end()) {
+                 cout<<item->second.direccion<<endl;
+                 
+                    }
+                }else if(opc1==2){
+                    auto item = datos_rep2.find(num);
+                         if (item != datos_rep2.end()) {
+                            cout<<"Instagram: "<<item->second.Instagram<<" Github: " <<item->second.Github <<endl;
+                        }
+                }else if(opc1==3){
+                    break;
+                }
+                }else{
+                    auto item = datos_no_rep.find(n);
+                         if (item != datos_no_rep.end()) {
+                             item->second.veces_visitado+=1;
+                        }
+                    int cerrar=1;
+                    while (cerrar==1){
+                    int opc;
+                    cout<<"Perfecto, quiere saber info de "<< n <<" sobre: (1:telefono)(2:direccion)(3:redes sociales)(4:salir)"<<endl;
+                    cin>>opc;
+                           
+                    if(opc==1){
+                        auto item = datos_no_rep.find(n);
+                         if (item != datos_no_rep.end()) {
+                            cout<<item->second.telefono<<endl;
+                        }
 
-mi_arbol.inOrden();
+                    }else if(opc==2){
+                        auto item = datos_no_rep.find(n);
+                         if (item != datos_no_rep.end()) {
+                            cout<<item->second.direccion<<endl;
+                        }
+                    }else if(opc==3){
+                        auto item = datos_no_rep.find(n);
+                         if (item != datos_no_rep.end()) {
+                            cout<<"Instagram: "<<item->second.Instagram<<" Github: " <<item->second.Github <<endl;
+                        }
+                        
+                    }
+                    
+                    else if(opc==4){
+                        cerrar=0;
+                       
+                    }
 
-vector<int>vec=mi_arbol.get_vector();
-
-// tener cuidado si hay un valor repetido poner condicion(modificar su count para que sea unico y mandarlo al)
-for (int i = 0; i < vec.size(); ++i) {
-    map1[vec[i]] = i;
+                }
+                }
+            } 
+        }if (opciones==3){
+        string cont;
+        cout<<"Ingrese el nombre del contacto que quiere eliminar"<<endl;
+        cin>>cont;
+        
+        //---------------- caso repetidos------------------
+        auto item = datos_rep.find(cont);
+        if (item != datos_rep.end()) {
+            if( item->second>=1){
+                string num2;
+                cout<<"se necesita informacion adicional, ingrese el numero del contacto"<<endl; // cuando el nombre es repetido
+                cin>>num2;
+                 auto item = datos_rep2.find(num2); // se busca en el mapa de repetidos el numero que se asocia al contacto que buscamos
+                 if (item != datos_rep2.end()) {
+                    hash.remove(item->second.nombre);
+                        }
+                    
+            }else{
+                 hash.remove(cont);
+            }
+            //--------------- caso no repetidos----------------
+        }
+    } if(opciones==4){
+         mi_arbol.display();
+    }if (opciones==5){
+        hash.display();
+    }if (opciones==6){
+        cout<<"Hay "<<mi_arbol.get_size()<<" Contactos en la libreta disponibles"<<endl;
+    }if(opciones==7){
+         mi_arbol.inOrden();
+        vector<string>vector2=mi_arbol.get_vector();
+            for (int i=0;i<vector2.size();i++){
+               auto item = datos_no_rep.find(vector2[i]);
+                         if (item != datos_no_rep.end()) {
+                            cout<<"{"<<"Nombre: "<<vector2[i]<<","<<"frecuencia de visitas del perfil: "<<item->second.veces_visitado<<"}"<<endl;
+                        }
+                        
+                }
+    }if (opciones==8){
+         mi_arbol.inOrden();
+         vector<string>vector=mi_arbol.get_vector();
+         ofstream archivo;
+        archivo.open("Copia_de_seguridad.txt",ios::out);// abrir archivo
+            if(archivo.fail()){
+                cout<<"no se pudo abrir el archivo";
+            }
+            for (int i=0;i<vector.size();i++){
+              // tambien hay que revisar si esta repedido :)
+                auto item = datos_no_rep.find(vector[i]);
+                         if (item != datos_no_rep.end()) {
+                            archivo<<"{"<<vector[i]<<" , "<<"Telefono: "<<item->second.telefono<<" , "<<"Direccion: "<<item->second.direccion<<","<<"Instagram: "<<item->second.Instagram<<","<<"Github: "<<item->second.Github<<","<<"# de veces que se visitó en perfil: "<<item->second.veces_visitado<<"}"<<endl;
+                        }
+                        
+                }
+            
+            archivo.close();
     }
 
+    else if (opciones==9){
+        salir =0;
+    }else if(opciones>8 or opciones<1){  // por si el usuario ingresa un comando invalido
+        salir=0;
+    }
 
-for(int i = 0; i < vec.size(); ++i){
- auto item = map1.find(vec[i]);
- auto item2=map2.find(vec[i]);
-    if (item != map1.end() and item2 != map2.end()) {
-        hash.insert(item2->second.nombre,item->second);
-    cout << "La llave existe  " << item->first << ";" << item->second << "}\n";
-    cout<<"llave 2 "<<item2->first<<";"<<item2->second.nombre<<endl;
-  } else {
-    cout << "La llave no existe" << endl;
-  }
 }
-
-//los nombres de la cola no estan ordenados
-    
-//podria no usar el hash y solo usar el vector ordenado
-
-hash.display();
-    //si hay una colision ( es decir que se agrega el mismo nombre, hay que hacer que el nodo donde esta el nombre repetido apunte a una lista de nombres repetidos)
-//mi_arbol.display(); 
-// el arbol está ordenado
-
-
 
 return 0;
 }
-// para que la tabla hash este organizada, encontrar un valor hash que al aplicarle el modulo de lo necesario para quedar ordenado
+
